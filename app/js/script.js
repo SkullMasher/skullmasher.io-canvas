b9d484d820126d1bdd97f6f7029c96d7ea425aaf
Florian Ledru <skullm4sher@gmail.com>
2017-05-31 19:56:24 +0200

    :skull: Fill the canvas with squares

--- app/js/script.js
+++ app/js/script.js
@@ -11,31 +11,29 @@ document.addEventListener('DOMContentLoaded', function (event) {
   var c = canvas.getContext('2d', {alpha: true})
 
   // Colors
-  var rectMainColor = '#ff0000'
+  var rectMainColor = '#262626'
 
   // Square Dimension
   var squareD = 6
 
   // spacing beetween squares
-  var spacing = 3
-
-  c.fillStyle = 'chartreuse'
-  c.fillRect(spacing, 50, squareD, squareD)
-  c.fillRect((spacing * 2) + squareD, 50, squareD, squareD)
-  c.fillRect((spacing * 3) + (squareD * 2), 50, squareD, squareD)
-  c.fillRect((spacing * 4) + (squareD * 3), 50, squareD, squareD)
+  var spacing = 2
 
   c.fillStyle = rectMainColor
-  // Create a line of squares
-  var i = 1
-  var x = 0
-  var y = spacing
-  while (x < canvas.width) {
-    x = (spacing * i) + (squareD * (i - 1))
-    y = spacing
-    c.fillRect(x, y, squareD, squareD)
+  // Fill the canvas with squares
+  var i, j, x, y
+  i = j = 1
+  x = y = 0
 
+  while (y < canvas.height) {
+    y = (spacing * i) + (squareD * (i - 1))
+    while (x < canvas.width) {
+      x = (spacing * j) + (squareD * (j - 1))
+      c.fillRect(x, y, squareD, squareD)
+
+      j++
+    }
+    j = x = 0
     i++
   }
-  console.log(i)
 })
