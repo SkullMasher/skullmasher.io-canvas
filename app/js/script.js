1beda61c7d286a50eedd26c9b9a3d5ee6b8bd757
Florian Ledru <skullm4sher@gmail.com>
2017-10-17 19:43:14 +0200

    :skull: Animation fuckery I made some black magic !

--- app/js/script.js
+++ app/js/script.js
@@ -1,14 +1,18 @@
-// Vanilla Javascript please
-document.addEventListener('DOMContentLoaded', function (event) {
+addEventListener('DOMContentLoaded', (event) => {
+  // Helper function
+  const getRandomIntFromRange = (min, max) => {
+    return Math.floor(Math.random() * (max - min) + min)
+  }
+
   const canvas = document.querySelector('#canvas')
 
   // Canvas size
   const canvasHeight = 300
-  canvas.width = window.innerWidth
+  canvas.width = innerWidth
   canvas.height = canvasHeight
 
   // set context of the canvas 2d or webGL
-  const c = canvas.getContext('2d', {alpha: true})
+  const ctx = canvas.getContext('2d', {alpha: true})
 
   // Colors
   const rectColor = '#262626'
@@ -20,45 +24,54 @@ document.addEventListener('DOMContentLoaded', function (event) {
   // spacing beetween squares
   const spacing = 2
 
-  c.fillStyle = rectColor
   // Fill the canvas with squares
-  let i, j, x, y
-  j = i = 1
-  x = y = 0
+  const squares = []
+  let fillCanvas = () => {
+    let i, j, x, y
+    j = i = 1
+    x = y = 0
 
-  // Fill the available space with rectangle
-  while (y < canvas.height) {
-    // Vertical line
-    y = (spacing * i) + (squareD * (i - 1))
-    while (x < canvas.width) {
-      // Horizontal line
-      x = (spacing * j) + (squareD * (j - 1))
-      c.fillRect(x, y, squareD, squareD)
+    // Clear the canvas
+    ctx.clearRect(x, y, canvas.width, canvas.height)
+    // Default squares color
+    ctx.fillStyle = rectColor
 
-      j++
+    while (y < canvas.height) {
+      // Vertical line
+      y = (spacing * i) + (squareD * (i - 1))
+      while (x < canvas.width) {
+        // Horizontal line
+        x = (spacing * j) + (squareD * (j - 1))
+        squares.push({x: x, y: y})
+        j++
+      }
+      j = x = 0
+      i++
     }
-    j = x = 0
-    i++
+    y = i = 0
+
+    // Draw all the rectangle
+    squares.forEach((sqr) => {
+      ctx.fillRect(sqr.x, sqr.y, squareD, squareD)
+    })
   }
-  y = i = 0
+  fillCanvas()
 
   // Make a rectangle breath
   let opacity = 0
   let opacityAccel = 0.01
   let breathAnimation = () => {
     requestAnimationFrame(breathAnimation)
-    // Draw a glowing green rectangle
-    x = y = 150
-    c.clearRect(x, y, squareD, squareD)
-    c.shadowColor = 'green'
-    // c.shadowBlur = 5
-    c.fillStyle = 'rgba(68,163,64,' + opacity + ')'
-    if (opacity > 1 || opacity < 0) {
+    // Choose a random rectangle
+    let x = squares[getRandomIntFromRange(0, squares.length)].x
+    let y = squares[getRandomIntFromRange(0, squares.length)].y
+    ctx.clearRect(x, y, squareD, squareD)
+    ctx.fillStyle = 'rgba(68,163,64,' + opacity + ')'
+    if (opacity > 1 || opacity < 0) {
       opacityAccel = -opacityAccel
     }
     opacity += opacityAccel
-    c.fillRect(x, y, squareD, squareD)
-    console.log(opacity)
+    ctx.fillRect(x, y, squareD, squareD)
   }
   breathAnimation()
 })
