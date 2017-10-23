2fcc0de70dbea9f39856c9a49ea0370ce0319292
Florian Ledru <skullm4sher@gmail.com>
2017-10-23 17:28:40 +0200

    :skull: global god count for better performance

--- app/js/script.js
+++ app/js/script.js
@@ -6,13 +6,19 @@ const random = (min, max) => {
 const canvas = document.querySelector('#canvas')
 
 // Canvas size
-const canvasHeight = innerHeight - document.querySelector('header').clientHeight
+// const canvasHeight = innerHeight - document.querySelector('header').clientHeight
+const canvasHeight = 300
 
 // set context of the canvas 2d or webGL
 const ctx = canvas.getContext('2d', {alpha: true})
 
 // Colors
 const rectColor = 'rgba(38,38,38,1)'
+const colorPool = [
+// { r: 38, g: 38, b: 38 }, // main grey color
+{ r: 54, g: 54, b: 54 }, // lighter grey color
+{ r: 69, g: 163, b: 64 } // matrix green grey color
+]
 // const rectHighlight = 'rgba(68,163,64,1)'
 
 // Square Dimension
@@ -21,15 +27,27 @@ const squareD = 6
 // spacing beetween squares
 const spacing = 2
 
+// Initialize some squares helper methods and variables
+let squaresLength = 0
+const squares = []
+
+/*
+ * Instead of doing a count on each square object we check if the lucky number
+ * of the object match the number in the god count to have much better
+ * performance. God count gets reset at the when it's egal to the number of
+ * of total squares on the screen is reach.
+ * The god Count is used by the square.breath prototype function.
+ */
+let godCount = 0
+
 function Square (x, y, opacity, opacityAccel) {
   this.x = x
   this.y = y
   this.color = rectColor
-  this.opacity = opacity || 1
-  this.opacityAccel = opacityAccel || 0.01
+  this.opacity = opacity || 0.5
+  this.opacityAccel = opacityAccel || -0.01
   this.squareIsBreathing = false
-  this.delay = random(50, 200)
-  this.count = 0
+  this.luckyNumber = random(0, squaresLength)
 }
 
 Square.prototype.draw = function () {
@@ -39,10 +57,9 @@ Square.prototype.draw = function () {
 
 Square.prototype.breath = function () {
   // Make a rectangle breath
-  this.count++
-  if (this.count === this.delay) {
+  if (godCount === this.luckyNumber) {
     this.squareIsBreathing = true
-    this.delay = random(50, 200)
+    this.luckyNumber = random(0, squaresLength)
   }
 
   if (this.squareIsBreathing) {
@@ -61,27 +78,19 @@ Square.prototype.breath = function () {
     this.squareIsBreathing = false
     this.opacityAccel = -this.opacityAccel // Get ready to glow again
     this.opacity = 0.2
-    this.count = 0
   }
 }
 
 // Fill the canvas with squares
-const squares = []
-
-const getRandomSquare = () => {
-  const squaresLength = squares.length
-  return squares[random(0, squaresLength)]
-}
-
 let fillCanvas = () => {
-  let i, j, x, y, square
+  let i, j, x, y
   j = i = 1
   x = y = 0
 
   // Clear the canvas
-  ctx.clearRect(x, y, canvas.width, canvas.height)
-  // Default squares color
-  ctx.fillStyle = rectColor
+  // ctx.clearRect(x, y, canvas.width, canvas.height)
+
+  ctx.fillStyle = rectColor // Default squares color
 
   while (y < canvas.height) {
     y = (spacing * i) + (squareD * (i - 1)) // Vertical line
@@ -99,6 +108,7 @@ let fillCanvas = () => {
   squares.forEach((sqr) => {
     sqr.draw()
   })
+  squaresLength = squares.length
 }
 
 let init = () => {
@@ -110,12 +120,11 @@ let init = () => {
 
 // Animaiton
 let animate = () => {
-  // instenciate some squares
-  // const randomSquare = getRandomSquare()
-  // ask them to breath
   squares.forEach((sqr) => {
     sqr.breath()
   })
+  if (godCount === squaresLength) { godCount = 0 }
+  godCount++
   requestAnimationFrame(animate)
 }
 
